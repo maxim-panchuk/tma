6715aa1c15bc0414124a104f8022459ee3365988
mpanchuk <maksim.panchuk29@gmail.com>
2024-10-07 22:51:09 +0300

    delete comments

--- server/websocket.go
+++ server/websocket.go
@@ -8,53 +8,46 @@ import (
 )
 
 type socket struct {
-	clients   map[*websocket.Conn]bool // Активные WebSocket соединения
-	mu        sync.Mutex               // Для безопасного параллельного доступа к клиентам
-	eventChan chan *event.EventDTO     // Канал для событий
+	clients   map[*websocket.Conn]bool
+	mu        sync.Mutex
+	eventChan chan *event.EventDTO
 }
 
 func newSocket() *socket {
 	return &socket{
 		clients:   make(map[*websocket.Conn]bool),
-		eventChan: event.Keeper().Ch, // Предполагаем, что у тебя есть структура Event
+		eventChan: event.Keeper().Ch,
 	}
 }
 
-// Метод для отправки события всем клиентам
 func (h *socket) broadcastEvent(event *event.EventDTO) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	// Проходим по всем клиентам и отправляем событие
 	for client := range h.clients {
 		err := websocket.JSON.Send(client, event)
 		if err != nil {
-			// Если произошла ошибка при отправке, закрываем соединение и удаляем клиента
 			client.Close()
 			delete(h.clients, client)
 		}
 	}
 }
 
-// WebSocket обработчик
 func (h *socket) updateEvent(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
-		// Добавляем нового клиента
 		h.mu.Lock()
 		h.clients[ws] = true
 		h.mu.Unlock()
 
 		defer func() {
-			// Удаляем клиента при закрытии соединения
 			h.mu.Lock()
 			delete(h.clients, ws)
 			h.mu.Unlock()
 			ws.Close()
 		}()
 
-		// Чтение данных из канала событий и рассылка клиентам
-		for event := range h.eventChan {
-			h.broadcastEvent(event)
+		for e := range h.eventChan {
+			h.broadcastEvent(e)
 		}
 	}).ServeHTTP(c.Response(), c.Request())
 	return nil
