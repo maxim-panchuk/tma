3718f2a415f73bd813b96cd82645ff8b744e0f11
a-zelenkov <zelenkov_a_m_01@mail.ru>
2024-10-08 20:14:00 +0300

    build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,16 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
+	},
+	build: {
+		outDir: '../server/static',
+		emptyOutDir: true,
 	},
 	server: {
 		host: true,
